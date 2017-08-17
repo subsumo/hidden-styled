2c08eec1a45903af3996bbc6b730e60839310b11
Jacob <info@subsumo.com>
2017-08-17 17:46:01 -0400

    Update Hide.js

--- src/Hide.js
+++ src/Hide.js
@@ -5,6 +5,7 @@ export const breakpoints = {
   sm: '@media screen and (min-width: 40em) and (max-width: 52em)',
   md: '@media screen and (min-width: 52em) and (max-width: 64em)',
   lg: '@media screen and (min-width: 64em)',
+  xl: '@media screen and (min-width: 84em)'
 }
 
 export const hidden = key => props => props[key] ? {
@@ -17,12 +18,14 @@ export const xs = hidden('xs')
 export const sm = hidden('sm')
 export const md = hidden('md')
 export const lg = hidden('lg')
+export const xl = hidden('xl')
 
 const Hide = styled.div([],
   xs,
   sm,
   md,
-  lg
+  lg,
+  xl
 )
 
 export default Hide
